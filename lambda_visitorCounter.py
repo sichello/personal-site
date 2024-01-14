e3d9403b470400ac1eb62fd538316de6b0020c4b
sichello <leesichello@gmail.com>
2024-01-13 22:32:34 -0800

    Added Lambda function for vistor counter API gateway intergation

---
+++ lambda_visitorCounter.py
@@ -0,0 +1,60 @@
+import json
+import boto3
+
+
+print('Loading function')
+dynamo = boto3.client('dynamodb')
+
+
+def respond(err, res=None):
+    return {
+        'statusCode': '400' if err else '200',
+        'body': str(
+            err) if err else json.dumps(res),
+        'headers': {
+            'Content-Type': 'application/json',
+        },
+    }
+
+
+
+def lambda_handler(event, context):
+    operation = event['httpMethod']
+    
+    key = {
+        'site':{'S':'sichello'}
+    }
+           
+    if operation == 'GET':
+        # Retrieve item from DynamoDB
+        response = dynamo.get_item(TableName='visitorCounter', Key=key)
+        
+
+    
+    
+    elif operation == 'POST':
+        print('In POST')
+        # Update item in DynamoDB to increment the 'count' attribute
+        updateResponse = dynamo.update_item(
+            TableName='visitorCounter',
+            Key=key,
+            UpdateExpression='SET #count = #count + :increment',
+            ExpressionAttributeNames={'#count': 'count'},
+            ExpressionAttributeValues={':increment': {'N': '1'}}
+        )
+        print(json.dumps(updateResponse))
+        response = dynamo.get_item(TableName='visitorCounter', Key=key)
+        
+    else:
+        return respond(ValueError('Unsupported method "{}"'.format(operation)))
+        
+    
+    # Check if the item exists
+    if 'Item' in response:
+        count = (response['Item']['count']['N'])
+        return respond(None, response['Item'])
+    else:
+        return respond(ValueError('Item not found'))
+    
+    
+
