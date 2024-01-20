9b1ec7e6a5f19b8124a4d4d207af8c0040af9d5f
sichello <leesichello@gmail.com>
2024-01-20 04:41:02 -0800

    restructure

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,16 +2,23 @@ resource "aws_s3_bucket" "sichello_bucket" {
   bucket = "sichello.com-terraform"
 }
 
-resource "aws_s3_bucket_website_configuration" "static_website" {
+resource "aws_s3_object" "website_dir" {
+  key    = "website"
+  bucket = aws_s3_bucket.sichello_bucket.id
+  source = "website"
+
+  tags = {
+    Env = "test"
+  }
+
+  resource "aws_s3_bucket_website_configuration" "static_website" {
   bucket = aws_s3_bucket.sichello_bucket.id
 
   index_document {
-    suffix = "index.html"
+    suffix = "site_resources/index.html"
   }
-
   error_document {
     key = "error.html"
   }
-  
 }
 
