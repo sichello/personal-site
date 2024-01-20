8b1915e463a1507cd829f2a0c2c016000ef5b77c
sichello <leesichello@gmail.com>
2024-01-20 05:05:53 -0800

    index

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,11 +4,14 @@ resource "aws_s3_bucket" "sichello_bucket" {
 
 resource "aws_s3_object" "provision_website_files" {
 
-  for_each = fileset("../website/", "**/*.*")
+#  for_each = fileset("../website/", "**/*.*")
+#  bucket = aws_s3_bucket.sichello_bucket.id
+#  key    = each.value
+#  source = "${each.value}"
 
-  bucket = aws_s3_bucket.sichello_bucket.id
-  key    = each.value
-  source = "${each.value}"
+  key                    = "index.html"
+  bucket                 = aws_s3_bucket.sichello_bucket.id
+  source                 = "../website/index.html"
 }
 
 resource "aws_s3_bucket_website_configuration" "static_website" {
