89c054a2857e38e8845f870ea0af651a82e42e95
sichello <leesichello@gmail.com>
2024-01-20 04:54:50 -0800

    missing "

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ resource "aws_s3_bucket" "sichello_bucket" {
 }
 
 resource "aws_s3_object" "website_dir" {
-  for_each = fileset("../website/, "**/*.*")
+  for_each = fileset("../website/", "**/*.*")
   bucket = aws_s3_bucket.sichello_bucket.id
   key    = each.key
   source = "website/${each.value}"
