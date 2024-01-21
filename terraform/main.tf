d10e742fbcd16394a6db3c04f73cbe31e1ba9b2d
sichello <leesichello@gmail.com>
2024-01-20 23:10:57 -0800

    s3 mods

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,17 +2,32 @@ resource "aws_s3_bucket" "sichello_bucket" {
   bucket = "sichello.com-terraform"
 }
 
-resource "aws_s3_object" "provision_website_files" {
+module "template_files" {
+  source = "hashicorp/dir/template"
 
-#  for_each = fileset("../website/", "**/*.*")
-#  bucket = aws_s3_bucket.sichello_bucket.id
-#  key    = each.value
-#  source = "${each.value}"
+  base_dir = "${path.module}/website"
+  template_vars = {
+    # Pass in any values that you wish to use in your templates.
+    # vpc_id = "vpc-abc123"
+  }
+}
 
-  key                    = "index.html"
-  bucket                 = aws_s3_bucket.sichello_bucket.id
-  source                 = "../website/index.html"
-  content_type           = "text/html"
+resource "aws_s3_bucket_object" "static_files" {
+  for_each = module.template_files.files
+
+  bucket       = aws_s3_bucket.sichello_bucket.id
+  key          = each.key
+  content_type = each.value.content_type
+
+  # The template_files module guarantees that only one of these two attributes
+  # will be set for each file, depending on whether it is an in-memory template
+  # rendering result or a static file on disk.
+  source  = each.value.source_path
+  content = each.value.content
+
+  # Unless the bucket has encryption enabled, the ETag of each object is an
+  # MD5 hash of that object.
+  etag = each.value.digests.md5
 }
 
 resource "aws_s3_bucket_website_configuration" "static_website" {
