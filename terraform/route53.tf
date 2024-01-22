90b1c5cd839e0d79e2d266585a66832dfe2ddecc
sichello <leesichello@gmail.com>
2024-01-22 14:45:07 -0800

    www r53

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -9,3 +9,15 @@ resource "aws_route53_record" "sichello_R53" {
     evaluate_target_health = false
   }
 }
+
+resource "aws_route53_record" "www_sichello_R53" {
+  zone_id = var.sichello_R53_zoneId
+  name    = www.${var.root_doamin_name}
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.cloudfront_distro.domain_name
+    zone_id                = aws_cloudfront_distribution.cloudfront_distro.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
