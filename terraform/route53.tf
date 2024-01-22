6e4ddd0b0b8b69317f80157b97587c22180bfcdd
sichello <leesichello@gmail.com>
2024-01-22 14:23:36 -0800

    r53

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -1,3 +1,7 @@
-resource "aws_route53_zone" "zone" {
-  name = "${var.root_doamin_name}-test"
+resource "aws_route53_record" "regional" {
+  zone_id = var.sichello_R53_zoneId
+  name    = var.root_doamin_name
+  type    = "A"
+  ttl     = "60"
+  records = ["${aws_cloudfront_distribution.cloudfront_distro.domain_name}"]
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,3 +3,9 @@ variable root_doamin_name {
   default     = "sichello.com"
   description = "Root domain name for the site"
 }
+
+variable sichello_R53_zoneId {
+  type        = string
+  default     = "Z024950516JAD3A13T2XU"
+  description = "Zone ID of existing sichello.com R53 hosted zone. Only want to modify existing zone to avoid deletion/recreation"
+}
