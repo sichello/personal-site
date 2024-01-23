57ed3279a85b7f4bf764c66ab9342979df1329a0
sichello <leesichello@gmail.com>
2024-01-22 23:18:17 -0800

    API

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -12,7 +12,8 @@ variable sichello_R53_zoneId {
 
 variable sichello_cert_arn {
   type        = string
-  default     = "arn:aws:acm:us-east-1:246445056940:certificate/4cd39535-53fd-4b05-bd34-18983ebf7baf"
+  # default   = "arn:aws:acm:us-east-1:246445056940:certificate/4cd39535-53fd-4b05-bd34-18983ebf7baf"
+  default     = "arn:aws:acm:ca-central-1:246445056940:certificate/3cb34fa0-0494-429a-8254-e76e65a6fbc2"
   description = "ARN for the sichello.com cert "
 }
 
