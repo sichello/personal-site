81baf307551818ae4f95b3447f4438a90cc91390
sichello <leesichello@gmail.com>
2024-01-22 21:31:02 -0800

    DB

--- terraform/dynamo.tf
+++ terraform/dynamo.tf
@@ -1,16 +1,31 @@
-resource "aws_dynamodb_table" "dynamodb-table" {
+# resource "aws_dynamodb_table" "dynamodb-table" {
+#   name           = "visitors"
+#   billing_mode   = "PAY_PER_REQUEST"
+#   hash_key       = "site"
+#   table_class    = "STANDARD_INFREQUENT_ACCESS"
+
+#   attribute {
+#     name = "site"
+#     type = "S"
+#   }
+# }
+module "dynamodb_table" {
+  source   = "terraform-aws-modules/dynamodb-table/aws"
+
   name           = "visitors"
   billing_mode   = "PAY_PER_REQUEST"
   hash_key       = "site"
   table_class    = "STANDARD_INFREQUENT_ACCESS"
 
-  attribute {
-    name = "site"
-    type = "S"
-  }
-
-    attribute {
+  attributes = [
+    {
+      name = "id"
+      type = "N"
+    },
+    {
     name = "count"
     type = "N"
   }
+  ]
 }
+
