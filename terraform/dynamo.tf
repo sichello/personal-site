ac7dbf9bd1c4f343cff0f951c354ae1f62b5d57d
sichello <leesichello@gmail.com>
2024-01-22 22:07:42 -0800

    DB

--- terraform/dynamo.tf
+++ terraform/dynamo.tf
@@ -3,17 +3,17 @@ resource "aws_dynamodb_table" "sichello-dynamodb-table" {
   billing_mode   = "PAY_PER_REQUEST"
   hash_key       = "site"
   table_class    = "STANDARD_INFREQUENT_ACCESS"
-  range_key      = "count"
+#   range_key      = "count"
 
 
   attribute {
     name = "site"
     type = "S"
   }
-  attribute{
-    name = "count"
-    type = "N"
-  }
+#   attribute{
+#     name = "count"
+#     type = "N"
+#   }
 }
 
 resource "aws_dynamodb_table_item" "sichello" {
@@ -21,9 +21,9 @@ resource "aws_dynamodb_table_item" "sichello" {
   hash_key   = aws_dynamodb_table.sichello-dynamodb-table.hash_key
 
   item = <<ITEM
-  {
-    "site": {"S": "sichello.com"},
-    "count": {"N": "0"},
-  }
-  ITEM
+{
+"site": {"S": "sichello.com"},
+"count": {"N": "0"},
+}
+ITEM
 }
