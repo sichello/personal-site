164325509c565da3f05473b60b1086f86f4fb0bc
sichello <leesichello@gmail.com>
2024-01-22 21:35:39 -0800

    DB

--- terraform/dynamo.tf
+++ terraform/dynamo.tf
@@ -1,31 +1,38 @@
-# resource "aws_dynamodb_table" "dynamodb-table" {
+resource "aws_dynamodb_table" "dynamodb-table" {
+  name           = "visitors"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "site"
+  table_class    = "STANDARD_INFREQUENT_ACCESS"
+  range_key      = "count"
+
+
+  attribute {
+    name = "site"
+    type = "S"
+  }
+  attribute{
+    name = "count"
+    type = "N"
+  }
+}
+
+# module "dynamodb_table" {
+#   source   = "terraform-aws-modules/dynamodb-table/aws"
+
 #   name           = "visitors"
 #   billing_mode   = "PAY_PER_REQUEST"
 #   hash_key       = "site"
 #   table_class    = "STANDARD_INFREQUENT_ACCESS"
 
-#   attribute {
-#     name = "site"
-#     type = "S"
-#   }
+#   attributes = [
+#     {
+#         name = "site"
+#         type = "S"
+#     },
+#     {
+#         name = "count"
+#         type = "N"
+#     }
+#   ]
 # }
-module "dynamodb_table" {
-  source   = "terraform-aws-modules/dynamodb-table/aws"
-
-  name           = "visitors"
-  billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "site"
-  table_class    = "STANDARD_INFREQUENT_ACCESS"
-
-  attributes = [
-    {
-        name = "site"
-        type = "S"
-    },
-    {
-        name = "count"
-        type = "N"
-    }
-  ]
-}
 
