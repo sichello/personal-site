3e6444a9c585af41f7366996100ebc5796c8043f
sichello <leesichello@gmail.com>
2024-01-22 22:34:33 -0800

    API

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -2,11 +2,11 @@ resource "aws_apigatewayv2_api" "sichello-visitors-API" {
   name          = "sichello-visitors-API"
   protocol_type = "HTTP"
 
-  cors_configuration = {
-    allow_headers     = "*"
-    allow_methods     = "*"
-    allow_origins     = "*"
-    expose_headers    = "*"
+  cors_configuration {
+    allow_headers     = ["*"]
+    allow_methods     = ["GET,POST"]
+    allow_origins     = ["*"]
+    expose_headers    = ["*"]
     max_age           = "300"
   }
 }
