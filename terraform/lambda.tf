90b52004e35988df7617babfff61410539e60550
sichello <leesichello@gmail.com>
2024-01-25 21:53:01 -0800

    lambda update

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -22,7 +22,7 @@ data "archive_file" "python_lambda_package" {
 
 resource "aws_lambda_function" "visitorAPI_lambda_function" {
         function_name = "visitorAPI"
-        filename      = "lambda_visitorAPI.zip"
+        filename      = "lambda_function.py"
         source_code_hash = data.archive_file.python_lambda_package.output_base64sha256
         role          = aws_iam_role.lambda_role.arn
         runtime       = "python3.10"
