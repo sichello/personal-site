00c8f60972aefe4202c25874462b741470a0b982
sichello <leesichello@gmail.com>
2024-01-25 22:00:30 -0800

    lambda

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -17,15 +17,15 @@ resource "aws_iam_role" "lambda_role" {
 data "archive_file" "python_lambda_package" {  
   type = "zip"  
   source_file = "../lambda_visitorAPI.py" 
-  output_path = "lmabda_function.py"
+  output_path = "lambda_visitorAPI.zip"
 }
 
 resource "aws_lambda_function" "visitorAPI_lambda_function" {
         function_name = "visitorAPI"
-        filename      = "lambda_function.py"
+        filename      = "lambda_visitorAPI.zip"
         source_code_hash = data.archive_file.python_lambda_package.output_base64sha256
         role          = aws_iam_role.lambda_role.arn
         runtime       = "python3.10"
-        handler       = "lambda_function.lambda_handler"
+        handler       = "lambda_visitorAPI.lambda_handler"
         timeout       = 10
 }
