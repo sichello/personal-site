ca355b494261d8c3a6776c718fd4654189c196b8
sichello <leesichello@gmail.com>
2024-01-25 23:11:58 -0800

    iam

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,31 +1,78 @@
-data "aws_iam_policy_document" "lambda_assume_role_policy" {
-  statement {
-    effect = "Allow"
-    actions = ["sts:AssumeRole"]
-    principals {
-      type        = "Service"
-      identifiers = ["lambda.amazonaws.com"]
-    }
-  }
+# data "aws_iam_policy_document" "lambda_assume_role_policy" {
+#   statement {
+#     effect = "Allow"
+#     actions = ["sts:AssumeRole"]
+#     principals {
+#       type        = "Service"
+#       identifiers = ["lambda.amazonaws.com"]
+#     }
+#   }
 
-  # statement {
-  #   effect    = "Allow"
-  #   resources = ["${aws_dynamodb_table.sichello-dynamodb-table.arn}"]
-  #   actions   = [
-  #     "dynamodb:DeleteItem",
-  #     "dynamodb:GetItem",
-  #     "dynamodb:PutItem",
-  #     "dynamodb:Scan",
-  #     "dynamodb:UpdateItem",
-  #   ]
-  # }
+#   statement {
+#     effect    = "Allow"
+#     resources = ["${aws_dynamodb_table.sichello-dynamodb-table.arn}"]
+#     actions   = [
+#       "dynamodb:DeleteItem",
+#       "dynamodb:GetItem",
+#       "dynamodb:PutItem",
+#       "dynamodb:Scan",
+#       "dynamodb:UpdateItem",
+#     ]
+#   }
+# }
+
+# resource "aws_iam_role" "lambda_role" {  
+#   name = "lambda-lambdaRole"  
+#   assume_role_policy = data.aws_iam_policy_document.lambda_assume_role_policy.json
+# }
+
+
+resource "aws_iam_policy" "dynamoDBLambdaPolicy" {
+  name = "DynamoDBLambdaPolicy"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "dynamodb:GetItem"
+          "dynamodb:DeleteItem",
+          "dynamodb:GetItem",
+          "dynamodb:PutItem",
+          "dynamodb:Scan",
+          "dynamodb:UpdateItem",
+        ]
+        Resource = [
+          aws_dynamodb_table.sichello-dynamodb-table.arn        
+        ]
+      }
+    ]
+  })
 }
 
+resource "aws_iam_role" "lambdaRole" {
+  name = "LambdaRole"
 
-resource "aws_iam_role" "lambda_role" {  
-  name = "lambda-lambdaRole"  
-  assume_role_policy = data.aws_iam_policy_document.lambda_assume_role_policy.json
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
 }
+
+resource "aws_iam_role_policy_attachment" "lambda-policy-attachment" {
+  role       = aws_iam_role.lambdaRole.name
+  policy_arn = aws_iam_policy.dynamoDBLambdaPolicy.arn
+}
+
 
 data "archive_file" "python_lambda_package" {  
   type = "zip"  
