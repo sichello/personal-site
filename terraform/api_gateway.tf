7d962800fc939bd1735d459c31cccda6ac5bb45f
sichello <leesichello@gmail.com>
2024-01-26 13:16:30 -0800

    perms

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -45,9 +45,19 @@ resource "aws_apigatewayv2_route" "visitors_route" {
   target    = "integrations/${aws_apigatewayv2_integration.visitors-integration.id}"
 }
 
-# Deploy it 
+# Stage it 
 resource "aws_apigatewayv2_stage" "prod_stage" {
   api_id      = aws_apigatewayv2_api.sichello-visitors-API.id
   name        = "prod"
   auto_deploy = true
+}
+
+# Permissions 
+resource "aws_lambda_permission" "api_gw" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.visitors_lambda_function.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_apigatewayv2_api.sichello-visitors-API.execution_arn}/*/*"
 }
