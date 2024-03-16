ba9c89540900c3f04e411c63ba0be8afc1022569
sichello <leesichello@gmail.com>
2024-03-15 17:16:05 -0700

    set DB overwrite false

--- terraform/dynamo.tf
+++ terraform/dynamo.tf
@@ -1,21 +1,22 @@
-# resource "aws_dynamodb_table" "sichello-dynamodb-table" {
-#   name           = "visitors"
-#   billing_mode   = "PAY_PER_REQUEST"
-#   hash_key       = "site"
-#   table_class    = "STANDARD_INFREQUENT_ACCESS"
+resource "aws_dynamodb_table" "sichello-dynamodb-table" {
+  name           = "visitors"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "site"
+  table_class    = "STANDARD_INFREQUENT_ACCESS"
 
-#   attribute {
-#     name = "site"
-#     type = "S"
-#   }
-# }
+  attribute {
+    name = "site"
+    type = "S"
+  }
+}
 
-# resource "aws_dynamodb_table_item" "sichello" {
-#   table_name = aws_dynamodb_table.sichello-dynamodb-table.name
-#   hash_key   = aws_dynamodb_table.sichello-dynamodb-table.hash_key
+resource "aws_dynamodb_table_item" "sichello" {
+  table_name = aws_dynamodb_table.sichello-dynamodb-table.name
+  hash_key   = aws_dynamodb_table.sichello-dynamodb-table.hash_key
 
-#   # item = jsonencode({
-#   #   "site": {"S": "sichello.com"},
-#   #   "count": {"N": "0"}
-#   # })
-# }
+  item = jsonencode({
+    "site": {"S": "sichello.com"},
+    "count": {"N": "0"}
+  })
+  overwrite = false
+}
